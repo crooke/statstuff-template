33c9df39a4a800edbe84c6fc6a92f1a3202e484e
crooke <eric.crook@gmail.com>
2016-10-11 09:32:21 -0500

    Updated index.php

--- index.php
+++ index.php
@@ -1,18 +1,36 @@
-<?php defined( '_JEXEC' ) or die( 'Restricted access' );?>
+<?php
+defined( '_JEXEC' ) or die( 'Restricted access' );
+$doc = JFactory::getDocument();
+$doc->addStyleSheet('templates/' . $this->template . '/css/style.css');
+$doc->addScript('/templates/' . $this->template . '/js/main.js', 'text/javascript');
+?>
 <!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>" >
-
+<html>
 <head>
-  <jdoc:include type="head" />
-  <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/system/css/system.css" type="text/css" />
-  <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/system/css/general.css" type="text/css" />
-  <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/css/template.css" type="text/css" />
-  <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template;?>/css/style.css" type="text/css" />
+    <link rel="stylesheet" href="/support/css/style.css" type="text/css" />
+    <script src="/support/js/main.js" type="text/javascript"></script>
 </head>
-
 <body>
-<jdoc:include type="modules" name="top" />
-<jdoc:include type="component" />
-<jdoc:include type="modules" name="bottom" />
+    <!-- main container -->
+    <div class='main_container'>
+        <!-- header -->
+        <div class='header'>Header</div>
+        <!-- mid container - includes main content area and right sidebar -->
+        <div class='mid_container'>
+          <!-- main content area -->
+         <div class='main_content_area'>
+             <jdoc:include type="modules" name="position-3" style="xhtml" />
+             <jdoc:include type="message" />
+             <jdoc:include type="component" />
+             <jdoc:include type="modules" name="position-2" style="none" />
+         </div>
+        </div>
+
+        <!-- footer -->
+        <div class='footer'>
+            Footer
+        </div>
+
+    </div>
 </body>
 </html>
